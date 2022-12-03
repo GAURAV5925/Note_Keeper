a9a395cb1b4cf965e00c4095d8a589fc2033cd5b
GAURAV5925 <jhagaurav5925@gmail.com>
2022-12-03 22:51:54 +0530

    First Update

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  const hello = 1;
+
+
+  return (
+    <div>
+      <div className="App"> Hello World</div>
+      <div className="App"> Hello World 2</div>
+    </div>
+  );
+}
+
+export default App;
